563b3f421431cb918c8e021475722941dea0fb1c
Jeremias Erbs <jerry@badabam.de>
2022-02-08 12:47:57 +0100

    Use map to create Rooms

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import Header from './Header.js';
 import Room from './Room.js';
 
 export default function App() {
+  const rooms = [
+    {
+      text: 'Küche',
+      description: 'Herdplatten nicht vergessen',
+      isDescriptionVisible: true,
+      isClean: false,
+    },
+    {
+      text: 'Wohnzimmer',
+      description: 'Staubwischen auch auf dem Fensterbrett',
+      isDescriptionVisible: true,
+      isClean: true,
+    },
+    {
+      text: 'Bad',
+      description: 'Eigener Lappen für das Klo!',
+      isDescriptionVisible: false,
+      isClean: false,
+    },
+  ];
+
   return (
     <main className="App">
       <Header>Happy Cleaning</Header>
-      <Room
-        text="Küche"
-        description="Herdplatten nicht vergessen"
-        isDescriptionVisible
-        isClean
-      />
-      <Room
-        text="Wohnzimmer"
-        description="Staubwischen auch auf dem Fensterbrett"
-        isDescriptionVisible
-      />
-      <Room text="Bad" description="Eigener Lappen für das Klo!" />
+      {rooms.map(({ text, description, isDescriptionVisible, isClean }) => (
+        <Room
+          key={text}
+          text={text}
+          description={description}
+          isDescriptionVisible={isDescriptionVisible}
+          isClean={isClean}
+        />
+      ))}
     </main>
   );
 }
