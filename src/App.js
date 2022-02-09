12ffabcd85feb89c31198c091e84cc21f210a8c5
Jeremias Erbs <jerry@badabam.de>
2022-02-09 15:40:24 +0100

    Make rooms a state and toggle from Room

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
 import './App.css';
 import Header from './Header.js';
 import Room from './Room.js';
+import { useState } from 'react';
 
 export default function App() {
-  const rooms = [
+  const [rooms, setRooms] = useState([
     {
       text: 'Küche',
       description: 'Herdplatten nicht vergessen',
-      isDescriptionVisible: true,
       isClean: false,
     },
     {
       text: 'Wohnzimmer',
       description: 'Staubwischen auch auf dem Fensterbrett',
-      isDescriptionVisible: true,
       isClean: true,
     },
     {
       text: 'Bad',
       description: 'Eigener Lappen für das Klo!',
-      isDescriptionVisible: false,
       isClean: false,
     },
-  ];
+  ]);
 
   return (
     <main className="App">
       <Header>Happy Cleaning</Header>
-      {rooms.map(({ text, description, isDescriptionVisible, isClean }) => (
-        <Room
-          key={text}
-          text={text}
-          description={description}
-          isDescriptionVisible={isDescriptionVisible}
-          isClean={isClean}
-        />
-      ))}
+      {rooms.map(
+        ({ text, description, isDescriptionVisible, isClean }, index) => (
+          <Room
+            key={text}
+            text={text}
+            description={description}
+            isClean={isClean}
+            toggleStatus={event => {
+              event.stopPropagation();
+
+              setRooms([
+                ...rooms.slice(0, index),
+                { ...rooms[index], isClean: !isClean },
+                ...rooms.slice(index + 1),
+              ]);
+            }}
+          />
+        )
+      )}
     </main>
   );
 }
--- src/Room.js
+++ src/Room.js
@@ -1,22 +1,24 @@
+import { useState } from 'react';
 import './Room.css';
 
-export default function Room({
-  text,
-  description,
-  isDescriptionVisible,
-  isClean,
-}) {
+export default function Room({ text, description, isClean, toggleStatus }) {
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+
   const statusClassName = isClean
     ? 'Room__status Room__status--clean'
     : 'Room__status Room__status--dirty';
 
   return (
-    <section className="Room">
+    <section onClick={toggleDescription} className="Room">
       <header className="Room__header">
         <h2 className="Room__heading">{text}</h2>
-        <div className={statusClassName}></div>
+        <button onClick={toggleStatus} className={statusClassName}></button>
       </header>
       <p hidden={!isDescriptionVisible}>{description}</p>
     </section>
   );
+
+  function toggleDescription() {
+    setIsDescriptionVisible(!isDescriptionVisible);
+  }
 }
